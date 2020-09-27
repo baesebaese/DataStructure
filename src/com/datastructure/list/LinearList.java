de4975e0efcbc2e25543b5e07ac1277e1ab1a319
moren <morendo90@gmail.com>
2020-09-27 18:34:22 +0900

    DataStructure_1.LinearList

---
+++ src/com/datastructure/list/LinearList.java
@@ -0,0 +1,44 @@
+package com.datastructure.list;
+
+public class LinearList {
+	
+	public static void main(String srgs[]) {
+		int sale[][][] = new int [][][] {
+			// 1팀 분기별 판매량
+			{
+				//2007년
+				{63, 84, 140, 130}, 
+				//2008년
+				{157, 209, 251, 312}
+			},
+			// 2팀 분기별 판매량
+			{
+				//2007년
+				{59, 80, 130, 135},
+				//2008년
+				{149, 187, 239, 310}			
+			}
+		};
+			
+		for(int i=0; i<2; i++) {
+			System.out.printf("< %d 팀 > %n", i + 1);
+			
+			for(int j=0; j<2; j++) {
+				int year = 2007;
+				System.out.printf("%d 년 %n", year);
+				
+				for(int k=0; k<4; k++) {
+					System.out.printf("%d/4 분기 : sale[%d][%d][%d] = %d %n", 
+							k+1, i, j, k, sale[i][j][k] );
+					System.out.println("---------------------------------");
+					
+				}
+				year++;
+				System.out.println();
+			}
+		}
+	
+		
+	}
+
+}
