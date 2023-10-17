dd900f139018e2ae669f9a149abbf10988c175ba
morendo <morendo90@gmail.com>
2023-10-17 22:36:10 +0900

    #2 - 동적배열 (Dynamic Array)

---
+++ DataStructure/DataStructure/Array/DynamicArray2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Array
+{
+    public class DynamicArray2
+    {
+        private object[] arr;
+        private const int GROWTH_FACTOR = 2;
+
+        public  int Count { get; private set; }
+
+        public int Capacity
+        {
+            get { return arr.Length; }
+
+        }
+
+        public DynamicArray2(int capacity = 16)
+        {
+            arr = new object[capacity];
+            Count = 0;
+        }
+
+        public void Add(object element) 
+        {
+            //배열이 차면 확장
+            if (Count >= Capacity) {
+                int newSize = Capacity * GROWTH_FACTOR;
+                var temp = new object[newSize];
+                for (int i = 0; i < arr.Length; i++) {
+                    temp[i] = arr[i];
+                }
+                arr = temp;
+
+            }
+
+            arr[Count] = element;
+            Count++;
+        }
+
+        public object Get(int index) {
+            if (index > Capacity - 1) {
+                throw new ApplicationException("Element not found");
+            }
+
+            return arr[index];
+        }
+    }
+}
